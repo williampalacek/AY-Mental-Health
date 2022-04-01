e7d8949a86a190af9bdd644e24f7f4ab9354a398
Glitch (mental-health-ay) <none>
2022-04-01 15:38:56 +0000

    Commented marjority of code

--- README.md
+++
@@ -1,4 +0,0 @@
-The purpose of this project was to create a functional website under the form of
-a hackathon.
-
-Credit for Nav Bar goes towards codeademy.
--- script.js
+++ script.js
@@ -1,14 +1,21 @@
+/*Document.query selector returns 
+the first element in the docuement that matches the selector
+meaning it finds the coresponding class in html*/
 const mainMenu = document.querySelector(".mainMenu");
 const closeMenu = document.querySelector(".closeMenu");
 const openMenu = document.querySelector(".openMenu");
 
+/*These will be called when specifed event is triggered*/
 openMenu.addEventListener("click", show);
 closeMenu.addEventListener("click", close);
 
+//Once event is triggered following styles are applied
 function show() {
+  //set css style
   mainMenu.style.display = "flex";
   mainMenu.style.top = "0";
 }
 function close() {
-  mainMenu.style.top = "-100%";
+  //Set css style
+  mainMenu.style.top = "-105%";
 }
